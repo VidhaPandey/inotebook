8acedc8dc415e509b3299f85dbd1c5fd7770aebd
VidhaPandey <pandeyvidhavp@gmail.com>
2025-04-02 23:24:54 +0530

    Setup express server

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Add this middleware
+app.use(express.json());
+
 // Connect to MongoDB
 connectToMongo();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
+//Available Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () => {
     console.log(`iNotebook backend listening on port ${port}`)
